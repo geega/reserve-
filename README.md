d3c049a7a3872855cdbe677405cdc1ee7245d087
Svyatoslav V <varpihovsky@gmail.com>
2026-02-21 23:14:21 +0200

    Add base logic

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # reserve-
+
+Run  server for development 
+
+```
+node node_modules/.bin/http-server
+
+```
+
+
+
+
+### Additional info 
+
+
+
+#### Fonts 
+
+https://thedigital.gov.ua/fonts
+
+https://www.dropbox.com/scl/fo/gylb537vfe58stu3wubg0/AMPXS4cao0-0UCPWYaSkXDM?rlkey=mkgtdslwk272lps89wb9hiqqq&e=3&dl=0
+
---
+++ pwa/public/assets/scripts/main.js
@@ -0,0 +1,14 @@
+        // ---------- Simple screen state ----------
+        // const screens = {
+        //     load: document.getElementById("screen-load"),
+        //     //  pin:  document.getElementById("screen-pin"),
+        //     main: document.getElementById("screen-main"),
+        // };
+
+        // function showScreen(name) {
+        //     Object.values(screens).forEach(s => s.classList.remove("is-active"));
+        //     console.log(screens[name]);
+        //     screens[name].classList.add("is-active");
+        // }
+
+        // setTimeout(() => showScreen("main"), 500);
